e95336c7edf4260b1fee6253de49a3264f4f1e2a
shubh220922 <shubh22.srivastava@gmail.com>
2023-10-02 09:50:23 +0530

    (commit 4) Added extra webpages

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template, send_from_directory, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -9,8 +9,7 @@ app = Flask(__name__)
 def hello_world():
     jobs = load_jobs_from_db()
     return render_template('/home.html',
-                           jobs=jobs,
-                           company_name="Jovian")
+                           jobs=jobs)
 
 
 @app.route("/favicon.ico")
@@ -23,6 +22,15 @@ def favicon():
 def list_jobs():
     jobs = load_jobs_from_db()
     return jsonify(jobs)
+
+
+@app.route("/job/<id>")
+def show_job(id):
+    job = load_job_from_db(id)
+    if not job:
+        return "Not Found", 404
+    return render_template('/jobpage.html',
+                           job=job)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -7,6 +7,28 @@ database_url = os.getenv("DATABASE_URL")
 engine = create_engine(database_url)
 
 
+def load_job_from_db(id_val):
+    with engine.connect() as conn:
+        query = (f"SELECT * " 
+                 f"FROM jobs "
+                 f"WHERE id = {id_val}")
+        result = conn.execute(text(query))
+        row = result.all()
+        if len(row) == 0:
+            return {}
+        elem = row[0]
+        result_dict = {
+            'id': elem[0],
+            'title': elem[1],
+            'location': elem[2],
+            'salary': elem[3],
+            'currency': elem[4],
+            'responsibilities': elem[5],
+            'requirements': elem[6]
+        }
+        return result_dict
+
+
 def load_jobs_from_db():
     with engine.connect() as conn:
         result = conn.execute(text("SELECT * FROM jobs"))
